b7b238402a2b0575ec9dca2e1f7de8a715faeb3c
DARLina <105904898+LinaDav17@users.noreply.github.com>
2022-11-12 23:18:02 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LAB1YAP
+{
+    class Program
+    {
+        static void Main()
+        {
+                int metr = int.Parse(Console.ReadLine());
+
+                while (metr >= 1000)
+                {
+                    metr = metr % 1000;
+                }
+                Console.WriteLine(1000 - metr);
+            
+        }
+    }
+}
