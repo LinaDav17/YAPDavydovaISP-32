18de78e7ce9888df528fa006fdec7e20c199c4e3
DARLina <105904898+LinaDav17@users.noreply.github.com>
2022-11-21 21:35:51 +0400

    Update LAB1.cs

--- Metod/LAB1.cs
+++ Metod/LAB1.cs
@@ -1,15 +1,1 @@
-using System;
-
-namespace ConsoleApp4
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string sl = Console.ReadLine();
-
-            for (int i = 0; i < sl.Length; i++)
-                Console.WriteLine(sl[i]);
-        }
-    }
-}
+Тут будет код потом)
