18a8fc489ff68647089a2badd9efa50134febb81
DARLina <105904898+LinaDav17@users.noreply.github.com>
2022-12-04 23:35:03 +0400

    Update LAB4.cs

--- Конструктор/LAB4.cs
+++ Конструктор/LAB4.cs
@@ -1,67 +1,100 @@
 using System;
-using System.Linq;
 
-
+namespace YAP
+{
     class Money
     {
-        public string _rub = "р.", _kop = "коп.";
-        public int rub, kop;
-        public Money() {  }
-        public Money(int r)
+        private int rub, kop;
+        private string _rub, _kop;
+
+        public Money() { }
+        public Money(string r, string _r)
         {
-            rub = r;
-            kop = 0;
-            if(kop > 99)
-            {
-                int ost = kop / 100;
-                rub = rub + ost;
-                kop = kop % 100;
-            }
-        }
-        public Money(int r, int k)
-        {
-            rub = r;
-            kop = k;
+            rub = int.Parse(r); ///
+            _rub = _r;
             if (rub < 0 || kop < 0)
                 Console.WriteLine("Не может быть отрицательным!");
-            else if (kop >99)
+        }
+        public Money(string r, string _r, string k, string _k)
+        {
+            rub = int.Parse(r); _rub = _r; ///
+            kop = int.Parse(k); _kop = _k; ///
+            if(_r != "р.")
+            {
+                Console.WriteLine("Рубли и копейки перепутаны местами!");
+            }
+            if (kop > 99)
             {
                 int ost = kop / 100;
                 rub = rub + ost;
                 kop = kop % 100;
             }
+            if (rub < 0 || kop < 0)
+                Console.WriteLine("Не может быть отрицательным!");
         }
         public void Print()
         {
             if (kop == 0)
-                Console.WriteLine($"Сумма: {rub} {_rub}");
+               Console.WriteLine($"{rub} {_rub}");
             else if (rub == 0)
-                Console.WriteLine($"Сумма: {kop} {_kop}");
+                Console.WriteLine($"{kop} {_kop}");
             else
-                Console.WriteLine($"Сумма: {rub} {_rub} {kop} {_kop}");
+                Console.WriteLine($"{rub} {_rub} {kop} {_kop}");
         }
-        //public static Money:Sum(Money a, Money b)
-        //{
 
-        //    return Money sum;
-        //}
-    }
-public class Program
-{
-    static void Main()
-    {
-        int rub = 0, kop = 0;
-        string _money = Console.ReadLine();
-        string[] kol = _money.Split(' ');
-        for(int i = 0; i < 4; ++i)
+        public static Money Sum(Money a, Money b)
         {
-            rub = int.Parse(kol[0]);
-            kop = int.Parse(kol[2]);
+            Money sum = new Money("0", "р.", "0", "коп.");
+
+            sum.rub = a.rub + b.rub;
+            sum.kop = a.kop + b.kop;
+                    
+            return sum;
         }
 
-        Money money = new Money(rub, kop);
-        Money money1 = new Money(rub);
-        money.Print();
-        money1.Print();
+        public static Money Difference(Money a, Money b)
+        {
+            Money diff = new Money("0", "р.", "0", "коп.");
+
+            diff.rub = a.rub - b.rub;
+            diff.kop = a.kop - b.kop;
+
+            return diff;
+        }
+        public void PrintTransferCost(double pr)
+        {
+            int koppr, kop1, kop2;
+
+            kop1 = rub * 100;
+            kop2 = kop1 + kop; 
+            koppr = (int)Math.Round(kop2 * pr);
+            kop = kop2 + koppr;
+
+            rub = kop / 100;
+
+            kop = kop % 100;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money money = new Money("10", "р.");
+            money.Print();
+            Money money2 = new Money("10", "р.", "101", "коп.");
+            money2.Print();
+            Console.WriteLine("Summ");
+            Money sum = Money.Sum(money, money2);
+            sum.Print();
+            Console.WriteLine("Diff");
+            Money a = new Money("100", "р.", "25", "коп.");
+            Money b = new Money("100", "р.", "5", "коп.");
+            Money diff = Money.Difference(a, b);
+            diff.Print();
+            Console.WriteLine("Transfer");
+            Money c = new Money("10", "р.", "50", "коп.");
+            c.PrintTransferCost(0.05);
+            c.Print();
+        }
     }
 }
